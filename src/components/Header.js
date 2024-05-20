3e85f21ea4433319efd4e66bb64521b95a6d8d10
ahan-stark <ahanabubakar@gmail.com>
2024-05-20 20:14:53 +0530

    added swiggy api for fetching restaurants

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import Title from "./Title";
-const Header = () => (
-  <div className="header">
-    <Title />
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+const Header = () => {
+  let [loginBtn, setloginBtn] = useState("Login");
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setloginBtn("Logout")
+                : setloginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
+      </div>
     </div>
-  </div>
+  );
+};
 
-);
 export default Header;
