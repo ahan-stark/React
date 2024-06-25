bcc5daa6983d0a52f26ca667c15feec8ff10405a
ahan-stark <ahanabubakar@gmail.com>
2024-06-25 23:55:54 +0530

    added test all dependency

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact No : 147852398</div>
+    <div >
+      <h1 className="text-orange-500 text-2xl font-bold text-center">Contact us</h1>
+      <div className="flex justify-center">
+      <form onSubmit={()=>{console.log("submited");}}>
+        <input className="border border-orange-300 m-4 mr-4" placeholder='mail'></input>
+        <input className="border border-orange-300 m-4 mr-4" placeholder='concerns'></input>
+        <button className="border bg-orange-300 p-1 rounded-lg h-7 text-white ">Submit</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
