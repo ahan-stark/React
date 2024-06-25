e3d7ac9b6c523586edd7eea3a0597c247c6bfeb7
ahan-stark <ahanabubakar@gmail.com>
2024-06-26 01:50:14 +0530

    added unit tests for Contact comp

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { render,screen } from "@testing-library/react"
-import Contact from "../Contact"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test('Should Load the Contact component', () => { 
-    render(<Contact/>);
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
- });
+test("Should Load the Contact component", () => {
+  render(<Contact />);
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
+});
+
+test("Checks if the mail placeholder is present is present", () => {
+  render(<Contact />);
+  const submit = screen.getByPlaceholderText("mail");
+  expect(submit).toBeInTheDocument();
+});
+test("should load 2 input boxes", () => {
+  render(<Contact />);
+  const twoInputs = screen.getAllByRole("textbox");
+  expect(twoInputs.length).toBe(2);
+});
